d0fbc248aa5698f6a7c5ef49ac8c1f0012eef72c
calemog <isaaccobbinah482@gmail.com>
2021-06-10 16:02:10 +0000

    lab 2 done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 #include "revert_string.h"
+
 
 void RevertString(char *str)
 {
-	// your code here
+    for(int i=0, l=strlen(str); i<l/2; i++)
+    {
+        char buff = str[i];
+        str[i] = str[l-i-1];
+        str[l-i-1] = buff;
+    }
 }
 
+// void __attribute__ ((constructor)) initLibrary(void) {
+//     printf("Library initialized \n");
+// }
+
+// void __attribute__ ((destructor)) cleanUpLibrary(void) {
+//     printf("Library is exited \n");
+// }
